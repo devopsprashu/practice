e819a54e36a5175186f83dcf7e76ce9d482a77b3
devopsprashu <prashansachincholkar@gmail.com>
2025-07-30 23:14:33 +0530

    messgae

--- first.tf
+++ first.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.8.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  # Configuration options
-}
 
 resource "azurerm_resource_group" "first" {
   name     = "first-rg"
